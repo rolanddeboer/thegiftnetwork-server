cc679fb5c52f66579d3c98783d3e6dc8f227a48c
Roland de Boer <roland+git@rolanddeboer.com>
2019-09-19 18:09:07 +0200

    Repairing build script

--- cl-build.sh
+++ cl-build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# set permissions: owner (roland) can write, group (hiawatha) can read or enter dir
-sudo chown roland:hiawatha /home/roland/sync-stage/thegiftnetwork -R
-find /home/roland/sync-stage/thegiftnetwork -type d -exec chmod 750 {} +
-find /home/roland/sync-stage/thegiftnetwork -type f -exec chmod 640 {} +
-
 # renew angular files
 rsync -a /home/roland/sync-stage/thegiftnetwork/* /var/www/thegiftnetwork.org/www/public --delete
 
+# set permissions: owner (roland) can write, group (hiawatha) can read or enter dir
+sudo chown roland:hiawatha /var/www/thegiftnetwork.org/www/public -R
+find /var/www/thegiftnetwork.org/www/public -type d -exec chmod 750 {} +
+find /var/www/thegiftnetwork.org/www/public -type f -exec chmod 640 {} +
+
 # copy over index html files to twig template that generates the eventual angular index page
-cp /var/www/thegiftnetwork.org/www/public/index.html /var/www/thegiftnetwork.org/www/templates/angular.html.twig -rf
+mv /var/www/thegiftnetwork.org/www/public/index.html /var/www/thegiftnetwork.org/www/templates/angular.html.twig -rf
